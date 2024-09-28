48d51e6c20e89d94aca0079fb9e077a573d76224
ultramyš <67806092+ultrasaks@users.noreply.github.com>
2024-09-28 09:48:41 +0200

    fixed nan

--- lab2/build/code.cpp
+++ lab2/build/code.cpp
@@ -4,15 +4,15 @@
 #include <cmath>
 
 int main(){
-    short a, b, c;
-    double answer;
+    unsigned short a, b, c;
+    float answer;
     std::cout << "Сторона A:" << std::endl;
     std::cin >> a;
     std::cout << "Сторона B:" << std::endl;
     std::cin >> b;
     std::cout << "Сторона C:" << std::endl;
     std::cin >> c;
-    answer = sqrt((a*a) + (b*b) + (c*c));
+    answer = sqrt((1.0*a*a) + (b*b) + (c*c));
     std::cout << "Главная диагональ - " << answer;
 
     return 0;
